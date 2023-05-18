3d9841611f9038605c6d5afa3fe98fcd117a6652
iDavid2619 <45333770+iDavid2619@users.noreply.github.com>
2023-05-18 12:31:28 +0800

    Update Application.java

--- src/main/java/com/david/springboot/Application.java
+++ src/main/java/com/david/springboot/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-//         adding comments
+//         adding comments mode
         SpringApplication.run(Application.class, args);
     }
 
